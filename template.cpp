58082a85cedc90dc74ee0f8541bdef9efed2a7cc
Galymzhan <115858999+uwbt17@users.noreply.github.com>
2024-12-17 00:24:55 +0500

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -19,67 +19,12 @@ void MAX(T& a, T b) { a < b ? a = b : a = a; }
 const int inf = (1ll << 31) - 1;
 const int INF = (1ll << 62) - 1 + (1ll << 62);
 const int N = 5e5 + 5;
-int n, qw, a[N], ql[N], qr[N], qk[N], ans[N];
-vector <int> has[N];
-vector <int> good[N];
-vector <int> pos[N];
-set <int> st[6];
-int cnt[N];
-void solve (int l, int r, vector <int> &cur) {
-	if (l > r || !szof(cur)) return;
-	int md = l + r >> 1;
-	vector <int> curL, curR;
-	for (int i = l; i <= r; i ++) has[i].clear();
-	for (int i : cur) {
-		if (ql[i] <= md && qr[i] > md) has[ql[i]].pb(i), has[qr[i]].pb(i);
-		else if (qr[i] > md) curR.pb(i);
-		else curL.pb(i);
-	}
-	for (int i = l; i <= md; i ++) cnt[a[i]] = 0;
-	for (int i = md; i >= l; i --) {
-		cnt[a[i]] ++;
-		for (int k = 2; k <= 5; k ++) {
-			st[k].insert(a[i]);
-			set <int> nw = st[k];
-			for (int c : st[k]) if (cnt[c] * k <= md - i + 1) nw.erase(c);
-			swap(st[k], nw);
-		}
-		for (int c : has[i]) for (int to : st[qk[i]]) good[c].pb(to);
-	}
-	for (int i = md + 1; i <= r; i ++) cnt[a[i]] = 0;
-	for (int i = md + 1; i <= r; i ++) {
-		cnt[a[i]] ++;
-		for (int k = 2; k <= 5; k ++) {
-			st[k].insert(a[i]);
-			set <int> nw = st[k];
-			for (int c : st[k]) if (cnt[c] * k <= i - md) nw.erase(c);
-			swap(st[k], nw);
-		}
-		for (int c : has[i]) for (int to : st[qk[i]]) good[c].pb(to);
-	}
-	solve(l, md, curL), solve(md + 1, r, curR);
-}
  
 void ka () {
-	cin >> n >> qw;
-	for (int i = 1; i <= n; i ++) cin >> a[i];
-	for (int i = 1; i <= qw; i ++) cin >> ql[i] >> qr[i] >> qk[i], ans[i] = inf;
-	vector <int> cur;
-	for (int i = 1; i <= qw; i ++) cur.pb(i);
-	solve(1, n, cur);
-	for (int i = 1; i <= n; i ++) pos[a[i]].pb(i);
-	for (int i = 1; i <= qw; i ++) {
-		for (int c : good[i]) {
-			int have = upper_bound(all(pos[c]), qr[i]) - lower_bound(all(pos[c]), ql[i]);
-			if (have * qk[i] > qr[i] - ql[i] + 1) MIN(ans[i], c);
-		}
-	}
-	for (int i = 1; i <= qw; i ++) {
-		cout << (ans[i] == inf ? -1 : ans[i]) << '\n';
-	}
+	
 }
 
-const bool uwbt17 = 1;
+const bool uwbt17 = 0;
 
 main() {
   cout.setf(ios::fixed);cout.precision(12);
