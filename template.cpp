7830117fb0afc144db27c14ce5c861a734b61137
Galymzhan <115858999+uwbt17@users.noreply.github.com>
2024-12-17 00:36:41 +0500

    Add files via upload

--- template.cpp
+++ template.cpp
@@ -27,14 +27,14 @@ void ka () {
 const bool uwbt17 = 0;
 
 main() {
-  cout.setf(ios::fixed);cout.precision(12);
-  ios_base::sync_with_stdio(0); cin.tie(0);
-  //freopen("mine.in" , "r", stdin); freopen("mine.out", "w", stdout);
-  #ifdef LOCAL
-  freopen("input.txt", "r", stdin);
-  #endif
-  int tc = 1; if (uwbt17) cin >> tc;
-  for (int cs = 1; cs <= tc; cs ++) {
+	cout.setf(ios::fixed);cout.precision(12);
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	//freopen("mine.in" , "r", stdin); freopen("mine.out", "w", stdout);
+	#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+	#endif
+	int tc = 1; if (uwbt17) cin >> tc;
+	for (int cs = 1; cs <= tc; cs ++) {
 		ka();
 		if (cs < tc) {
 			cout << '\n';
@@ -43,8 +43,8 @@ main() {
 			#endif
 		}
 	}
-  #ifdef LOCAL
-  cout << "\n\nTime elapsed: " << double(clock()) / CLOCKS_PER_SEC << " s.";
-  #endif
+	#ifdef LOCAL
+	cout << "\n\nTime elapsed: " << double(clock()) / CLOCKS_PER_SEC << " s.";
+	#endif
 }
 
